e19799df2f15e94e2ee67b6bf66545b3f00a4d58
taigaSugie <tqq15tqq@yahoo.co.jp>
2020-04-30 19:53:07 +0900

    20200430追加2

--- PostalCode/js/main.js
+++ PostalCode/js/main.js
@@ -1,10 +1,15 @@
 'use-strict'
 {
     const first = document.getElementById('first');
+    //　最初の3桁入力フォーム
     const second = document.getElementById('seccond');
+    // 4桁入力フォーム
     const postNumber = [ first, second ];
+    //　入力フォームまとめ
 
     postNumber() {
+        //　もし数字以外が入力されたら
         if()
+        //　入力させない
     }
 }
