85c5ceb784d5231da26073282889e9a90ac2b655
taigaSugie <tqq15tqq@yahoo.co.jp>
2020-05-01 20:03:20 +0900

    js修正未完

--- PostalCode/js/main.js
+++ PostalCode/js/main.js
@@ -7,9 +7,15 @@
     const postNumber = [ first, second ];
     //　入力フォームまとめ
 
-    postNumber() {
+    function number() {
         //　もし数字以外が入力されたら
-        if()
-        //　入力させない
+        if(document.forms.first.first === isNaN('abc')){
+            //　赤い背景を表示して警告
+            first.classList.add('red');
+        }else {
+            // 赤い背景クラスを外す
+            first.classList.remove('red');
+        }
     }
+    number();
 }
---
+++ SelectBox/js/main.js
@@ -0,0 +1,8 @@
+'use-strict'
+{
+    // selectboxを取得
+    // selectbox配下のoptionが選択されたら
+    // selectboxに表示
+    // バツボタンを押したら
+    // selectboxから消す
+}
