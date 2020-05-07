13de0e6bf0efcce03b66fadf59e3024f9aa3e0ff
taigaSugie <tqq15tqq@yahoo.co.jp>
2020-05-07 19:50:38 +0900

    郵便番号終了

--- PostalCode/js/main.js
+++ PostalCode/js/main.js
@@ -1,21 +1,29 @@
 'use-strict'
 {
-    const first = document.getElementById('first');
-    //　最初の3桁入力フォーム
-    const second = document.getElementById('seccond');
-    // 4桁入力フォーム
-    const postNumber = [ first, second ];
-    //　入力フォームまとめ
+    // formタグを読み込む
+   const input = document.getElementById('post1');
+   // 最初のinputタグを読み込む
+   const log1 = document.getElementById('first');
+   // ２番目のinputタグを読み込む
+   const log2 = document.getElementById('second');
 
-    function number() {
-        //　もし数字以外が入力されたら
-        if(document.forms.first.first === isNaN('abc')){
-            //　赤い背景を表示して警告
+   //　inputにkeyを押し上げたときにlogkeyを実行
+   input.onkeyup = logKey;
+
+    function logKey(e) {
+        // もしlog1で数字以外が押し込まれたら
+        if(log1.value.match(/[^0-9]+/)){
+            // redクラスを追加
             first.classList.add('red');
-        }else {
-            // 赤い背景クラスを外す
+        // もしlog2で数字以外が押し込まれたら
+        }else if(log2.value.match(/[^0-9]+/)){
+            // redクラスを追加
+            second.classList.add('red');
+        // 数字が押し込まれている状態なら
+        }else{
+            // redクラスを取り除く
             first.classList.remove('red');
+            second.classList.remove('red');
         }
     }
-    number();
 }
