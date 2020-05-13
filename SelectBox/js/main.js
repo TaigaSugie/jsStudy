02ff4f7ce0834f9c1a45d8a86c950863f2876c30
taigaSugie <tqq15tqq@yahoo.co.jp>
2020-05-13 18:33:36 +0900

    複数選択途中

--- SelectBox/js/main.js
+++ SelectBox/js/main.js
@@ -1,8 +1,22 @@
 'use-strict'
 {
     // selectboxを取得
+    var selectBox = document.getElementsByName('selectBox');
+    var selectCon = document.getElementsByName('selectContent');
+    var contentLength = selectCon.length;
+    // styleが
     // selectbox配下のoptionが選択されたら
+    selectCon.onclick = function(){
+        for(var i = 0; i < contentLength; i++){
+            if(selectCon.selected){
+                selectBox.textContent = this.value;
+            }else{
+                selectBox.textContent = this.value.disabled;
+            }
+        }
+    };
     // selectboxに表示
     // バツボタンを押したら
     // selectboxから消す
+    selectCon.onclick();
 }
