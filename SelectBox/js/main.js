929a4cf1b63df1aab0c8350d2f43d4118243a875
taigaSugie <tqq15tqq@yahoo.co.jp>
2020-05-19 11:41:07 +0900

    css制御不明

--- SelectBox/js/main.js
+++ SelectBox/js/main.js
@@ -1,23 +1,35 @@
 'use-strict'
 {
-    // selectboxを取得
-    var selectBox = document.getElementsByName('selectBox');
-    var selectCon = document.getElementsByName('selectContent');
-    var contentLength = selectCon.length;
-    // styleが
-    // selectbox配下のoptionが選択されたら
-    selectCon.onclick = function(){
-        for(var i = 0; i < contentLength; i++){
-            if(selectCon.selected){
-                // selectboxに表示
-                selectBox.textContent = this.value;
-            }
-            // バツボタンを押したら
-            if(){
-                selectBox.textContent = this.value.disabled;
-            }
+    // // selectboxを取得
+    // var selectBox = document.getElementsByName('selectBox');
+    // var selectCon = document.getElementsByName('selectContent');
+    // var contentLength = selectCon.length;
+    // // style指定
+    // // selectbox配下のoptionが選択されたら
+    // selectCon.onclick = function(){
+    //     for(var i = 0; i < contentLength; i++){
+    //         if(selectCon.selected){
+    //             // selectboxに表示
+    //             selectBox.textContent = this.value;
+    //         }
+    //         // バツボタンを押したら
+    //         if(){
+    //             selectBox.textContent = this.value.disabled;
+    //         }
+    //     }
+    // };
+    // // selectboxから消す
+    // selectCon.onclick();
+
+    var box = document.getElementById('box');
+    var listWrap = document.getElementsByClassName('listWrap');
+    // listWrap.style.display = "none";
+
+    box.onclick = function(){
+        if(listWrap.style.display == 'none'){
+            listWrap.style.display = 'block';
+        }else{
+            listWrap.style.display = 'none';
         }
     };
-    // selectboxから消す
-    selectCon.onclick();
 }
